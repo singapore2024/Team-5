25340ad65487dbd259599a904d796bd5b7926931
liuzehui03 <liuzehui03@gmail.com>
2024-10-11 14:09:21 +0800

    added header and menu page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Menu from './pages/Menu'
+// src/App.tsx
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./parts/Header"; // Import the Header component
+import Menu from "./pages/Menu";
+//import AdminPage from "./pages/Admin";
+//import OrdersPage from "./pages/Orders";
+import "./App.css"; // Include your global styles
 
 function App() {
   return (
-    // <AuthProvider>
-      <Router>
+    <Router>
+      {/* Render the Header component */}
+      <Header title="Fortitude Culina" />
+      <div > {/* Adjust the content to account for the sidebar */}
         <Routes>
+          <Route path="/" element={<Menu />} /> {/* Menu as the landing page */}
           
-          <Route path="/" element={<Menu />} />
         </Routes>
-      </Router>
-    // </AuthProvider>
-  )
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
