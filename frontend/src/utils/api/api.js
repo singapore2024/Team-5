63477d9ccac32c4123a591cd374aec8866d66ae3
jerlpjgithub <113880753+jerlpjgithub@users.noreply.github.com>
2024-10-11 12:17:23 +0800

    feat(menu): set up api call for menu

---
+++ frontend/src/utils/api/api.js
@@ -0,0 +1,27 @@
+import axiosInstance from "./axios";
+
+/**
+ * Fetches all menu items from the server
+ * 
+ * @returns {Promise} Promise object represents the response from the server
+ * 
+ *  The response is an array of menu items, where each menu item has the following structure:
+ * [
+ *     {
+ *         "name": "Dish Name",
+ *         "ingredients": [
+ *             {
+ *                 "quantity": "Quantity",
+ *                 "name": "Ingredient Name",
+ *                 "id": Ingredient ID
+ *             },
+ *             ...
+ *         ],
+ *         "id": Menu Item ID
+ *     },
+ *     ...
+ * ]
+ */
+export const getAllMenu = async () => {
+    return axiosInstance.get('/menu/all');
+}
